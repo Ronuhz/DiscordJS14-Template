e15c836c0808b8ae17579fbd928f65be48e91446
Ronuhz <110656577+Ronuhz@users.noreply.github.com>
2022-09-05 17:33:00 +0300

    Added Autocomplete Slash Command Example

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -66,6 +66,19 @@ module.exports = {
           ephemeral: true,
         });
       }
+    } else if (
+      interaction.type == InteractionType.ApplicationCommandAutocomplete
+    ) {
+      const { commands } = client;
+      const { commandName } = interaction;
+      const command = commands.get(commandName);
+      if (!command) return;
+
+      try {
+        await command.autocomplete(interaction, client);
+      } catch (error) {
+        console.log(error);
+      }
     }
   },
 };
